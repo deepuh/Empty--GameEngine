2f750ef36bd71acd7f80923195c3600bfd9ff4b6
Deep <115367789+deepuh@users.noreply.github.com>
2023-01-11 18:25:22 -0800

    Add files via upload

---
+++ Kamehameha.js
@@ -0,0 +1,13 @@
+class Kamehameha {
+    constructor() {
+
+    };
+
+    update() {
+
+    };
+
+    draw(ctx) {
+        ctx.drawImage(ASSET_MANAGER.getAsset("./kamehameha.png"), 0, 0);
+    };
+}
